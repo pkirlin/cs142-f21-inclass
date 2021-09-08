3c6d140db374f6e746b204c67203dd063870c93c
Phil Kirlin <pkirlin@gmail.com>
2021-09-08 12:56:37 -0500

    12pm

--- src/intro3/FunctionPractice.java
+++ src/intro3/FunctionPractice.java
@@ -20,10 +20,28 @@ public class FunctionPractice {
         return sum;
     }
 
+    public static double totalDistance(int[] points)
+    {
+        double sum = 0;
+        for (int i = 0; i < points.length - 2; i+=2)
+        {
+            int x = points[i];
+            int y = points[i+1];
+            int nextX = points[i+2];
+            int nextY = points[i+3];
+            System.out.println(x + " " + y);
+            double dist = distance(x, y, nextX, nextY);
+            System.out.println("distance is " + dist);
+            sum += dist;
+        }
+        return sum;
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("SumRange A: " + sumRange(1, 9));
-        System.out.println("SumRange B: " + sumRange(5, 15));
+        System.out.println(totalDistance(new int[] {1,2,3,-4,-3,0,7,1}));
+        //System.out.println("SumRange A: " + sumRange(1, 9));
+        //System.out.println("SumRange B: " + sumRange(5, 15));
         // Test your functions here.
         //System.out.println("Distance A: " + distance(1, 2, 3, -4));
         //System.out.println("Distance B: " + distance(0, 0, 0, 3));
