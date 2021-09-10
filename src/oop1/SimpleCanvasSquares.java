41e83144c6f7309f5861f90c0d586a159e5ea6bf
Phil Kirlin <pkirlin@gmail.com>
2021-09-10 14:19:37 -0500

    1pm sep 10

--- src/oop1/SimpleCanvasSquares.java
+++ src/oop1/SimpleCanvasSquares.java
@@ -9,5 +9,15 @@ public class SimpleCanvasSquares {
         // a concentric square design similar to this:
         // https://www.levygorvy.com/works/frank-stella/
 
+        SimpleCanvas squareCanvas = new SimpleCanvas(500, 500);
+        squareCanvas.drawFilledRectangle(100, 100, 300, 300);
+
+        Color mycolor = new Color(188, 218, 86);
+        squareCanvas.setPenColor(mycolor);
+        squareCanvas.drawFilledRectangle(150, 150, 200, 200);
+
+
+        squareCanvas.show();
+
     }
 }
