879e979d02621bed37ad5396e1e473762899de4a
Phil Kirlin <pkirlin@gmail.com>
2021-09-10 12:52:58 -0500

    12pm

--- src/oop1/SimpleCanvasFades.java
+++ src/oop1/SimpleCanvasFades.java
@@ -17,5 +17,9 @@ public class SimpleCanvasFades {
         // they want to see at the bottom of the screen, and make your canvas fade
         // from black at the top to whatever color the user wants at the bottom.
 
+        for (int i = 0; i < 256; i++)
+        {
+
+        }
     }
 }
--- src/oop1/SimpleCanvasSquares.java
+++ src/oop1/SimpleCanvasSquares.java
@@ -5,9 +5,16 @@ import java.awt.*;
 public class SimpleCanvasSquares {
     public static void main(String[] args) {
 
-        // Use SimpleCanvas to open a new canvas and draw
-        // a concentric square design similar to this:
-        // https://www.levygorvy.com/works/frank-stella/
+        SimpleCanvas squareCanvas = new SimpleCanvas(500, 500);
+        Color otherColor = new Color(50, 100, 200);
 
+        squareCanvas.setPenColor(otherColor);
+        squareCanvas.drawFilledRectangle(100, 100, 300, 300);
+
+        Color c = new Color(118, 64, 142);
+
+        squareCanvas.setPenColor(c);
+        squareCanvas.drawFilledRectangle(150, 150, 200, 200);
+        squareCanvas.show();
     }
 }
