3d4bb11654bf34fe3b69de5e7b95455a92b7761c
Phil Kirlin <pkirlin@gmail.com>
2021-09-24 12:57:14 -0500

    sep 24

--- src/oop2/Dog.java
+++ src/oop2/Dog.java
@@ -4,11 +4,48 @@ public class Dog {
     public String name;
     public int weight;
     public int age;
+    private int energy;
+
+    public Dog()
+    {
+        age = 0;
+        weight = 0;
+        name = "";
+        energy = 5;
+    }
+
+    public Dog(String newName, int newAge, int newWeight)
+    {
+        name = newName;
+        age = newAge;
+        weight = newWeight;
+        energy = 5;
+    }
+
+    public int getEnergy()
+    {
+        return energy;
+    }
+
+    public void setEnergy(int newEnergy)
+    {
+        if (newEnergy >= 0) {
+            energy = newEnergy;
+        }
+    }
 
     public String toString()
     {
-        String answer = name + " is " + age + " years old.";
+        String answer = name + " is " + age + " years old, and has energy= " + energy;
         return answer;
+    }
+
+    public void playFetch()
+    {
+        if (energy > 0) {
+            System.out.println(name + " is playing fetch!");
+            energy--;
+        }
     }
 
     public void speak()
@@ -26,6 +63,11 @@ public class Dog {
     public void speak(String whatToSay)
     {
         System.out.println(name + " says " + whatToSay);
+    }
+
+    public void playWith(Dog friend)
+    {
+        System.out.println(name + " is playing with " + friend.name);
     }
 
 }
--- src/oop2/DogDemo.java
+++ src/oop2/DogDemo.java
@@ -3,16 +3,28 @@ package oop2;
 public class DogDemo {
     public static void main(String[] args)
     {
-        Dog fido = new Dog();
-        fido.age = 3;
-        fido.name = "Fido";
-        fido.weight = 30;
-        Dog lassie = new Dog();
-        lassie.age = 5;
-        lassie.weight = 70;
-        lassie.name = "Lassie";
+        Dog fido = new Dog("Fido", 3, 30);
+        //fido.age = 3;
+        //fido.name = "Fido";
+        //fido.weight = 30;
+        Dog lassie = new Dog("Lassie", 5, 70);
+        //lassie.age = 5;
+        //lassie.weight = 70;
+        //lassie.name = "Lassie";
         System.out.println(fido);
         System.out.println(lassie);
+
+        fido.playFetch();
+        fido.playFetch();
+        fido.playFetch();
+        fido.playFetch();
+        fido.playFetch();
+        fido.playFetch();
+        System.out.println(fido);
+
+        fido.playWith(lassie);
+        lassie.playWith(fido);
+
 
         Dog dog3 = fido;
         dog3.age++;
