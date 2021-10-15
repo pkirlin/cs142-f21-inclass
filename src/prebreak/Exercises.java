4a9ffae6584981d8566de92e57c8c56150412028
Phil Kirlin <pkirlin@gmail.com>
2021-10-15 11:33:52 -0500

    oct 13

--- src/prebreak/Exercises.java
+++ src/prebreak/Exercises.java
@@ -1,6 +1,7 @@
 package prebreak;
 
 import java.io.InputStream;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,16 +9,37 @@ public class Exercises {
 
     public static void main(String[] args)
     {
-        readWords();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(2);
+        list.add(4);
+        list.add(6);
+        list.add(8);
+        list.add(10);
+        reverseArrayList(list);
+        System.out.println(list);
+        String mystring = "phone";
+        reverseString(mystring);
     }
 
-    public void reverseArrayList(ArrayList<Integer> list)
+    public static void reverseArrayList(ArrayList<Integer> list)
     {
-
+        int j = list.size() - 1;
+        for (int i = 0; i < list.size()/2; i++)
+        {
+            int temp = list.get(i);
+            list.set(i, list.get(j));
+            list.set(j, temp);
+            System.out.println("swap index " + i + " with " + j);
+            j--;
+        }
     }
 
-    public String reverseString(String str)
+    public static String reverseString(String str)
     {
+        for (int i = str.length() - 1; i >= 0; i--)
+        {
+            System.out.println(str.charAt(i));
+        }
         return "";
     }
 
