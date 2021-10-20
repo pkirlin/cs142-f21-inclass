30893a0743104b4f297d8e946ef2d54cf35601b4
Phil Kirlin <pkirlin@gmail.com>
2021-10-20 18:01:45 -0500

    oct 20

--- src/recursion/Recursion1.java
+++ src/recursion/Recursion1.java
@@ -4,21 +4,43 @@ public class Recursion1 {
 
     public static void main(String[] args)
     {
+        System.out.println(reverseRec("abcd"));
+
+
         for (int i = 1; i <= 10; i++)
         {
             long factAnswer = fact(i);
+            System.out.println("The factorial of " + i + " is " + factAnswer);
+        }
+        for (int i = 1; i <= 10; i++)
+        {
+            long factAnswer = factRec(i);
             System.out.println("The factorial of " + i + " is " + factAnswer);
         }
     }
 
     public static long fact(int num)
     {
-        return 0; // remove this when you start coding
+        long product = 1;
+        for (int i = 1; i <= num; i++)
+        {
+            product = product * i;
+        }
+        return product;
     }
 
     public static long factRec(int num)
     {
-        return 0; // remove this when you start coding
+        if (num == 1) // base case
+        {
+            return 1;
+        }
+        else // recursive case
+        {
+            long recursiveCall = factRec(num - 1);
+            return recursiveCall * num;
+            // return factRec(num-1) * num;
+        }
     }
 
     public static String reverse(String str)
@@ -33,6 +55,15 @@ public class Recursion1 {
 
     public static String reverseRec(String str)
     {
-        return ""; // remove this when you start coding
+        if (str.length() == 1)
+        {
+            return str;
+        }
+        else {
+            char lastChar = str.charAt(str.length()-1);
+            String revSmaller = reverseRec(str.substring(0, str.length()-1));
+            String answer = lastChar + revSmaller;
+            return answer;
+        }
     }
 }
