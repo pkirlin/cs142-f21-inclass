dff2d1fde7296f092b16b2521e32f2c3545aa4c9
Phil Kirlin <pkirlin@gmail.com>
2021-12-01 11:48:41 -0600

    dec1

---
+++ src/bigoh/Array2DSumTimer.java
@@ -0,0 +1,30 @@
+package bigoh;
+
+public class Array2DSumTimer extends CodeTimer {
+
+    public double measureTimeOnOneSize(int inputSize) {
+        // Create a 2D array of random numbers.  The size of the array
+        // is "inputSize" rows by "inputSize" columns
+        // and the numbers range from 0 to maxNumber (10,000 here).
+        int[][] randomNums = RandomArrayMaker.makeRandom2DArray(inputSize, 10000);
+
+        Stopwatch watch = new Stopwatch();
+        watch.start();
+
+        // You may need to modify the 1000 here if the code runs too fast or too slow.
+        for (int x = 0; x < 1000; x++) { // for loop that runs the code inside many times
+
+            // Code here is our normal algorithm to add up the numbers in an array.
+            int sum = 0;
+            for (int row = 0; row < randomNums.length; row++) {
+                for (int col = 0; col < randomNums[row].length; col++) {
+                    sum += randomNums[row][col];
+                }
+            }
+
+        }
+
+        watch.stop();
+        return watch.getElapsedTimeInSeconds();
+    }
+}
